bff19d42f6d8ea51718c40e8b7bbdc6f0be31c49
ilyakoptev <ilya.koptev@gmail.com>
2019-12-02 14:26:07 +0200

    add first redux state update with number

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+
+
+import { createStore } from "redux";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "ADD":
+            state = state + action.payload
+            break;
+        case "SUBSTRACT":
+            state = state - action.payload
+            break;
+    }
+    return state;
+}
+
+const store = createStore(reducer, 1);
+
+store.subscribe(() => {
+    console.group("Store updated", store.getState())
+})
+
+store.dispatch({
+    type: "ADD",
+    payload: 10
+})
+
+store.dispatch({
+    type: "ADD",
+    payload: 100
+})
+
+store.dispatch({
+    type: "SUBSTRACT",
+    payload: 80
+})
